bb6090f9f3827308c6394bd0c6c6971a587cce9b
Sardor Sayidaxmedov <sardor.sayidakhmedov@gmail.com>
2022-12-15 23:41:07 +0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
+import { useEffect, useState } from "react"
 import Head from "next/head"
-import { GraphQLClient, gql } from "graphql-request"
-import { gql as apollo } from "@apollo/client"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 import Table from "@mui/material/Table"
@@ -11,36 +10,92 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import Paper from "@mui/material/Paper"
 
-const graphcms = new GraphQLClient(
-  "https://api-ap-south-1.hygraph.com/v2/clbnux5qh1k8r01t8b9jnhrco/master"
-)
+export default function Home() {
+  const [students, setStudents] = useState([])
+  const [loading, setLoading] = useState(false)
 
-const QUERY = gql`
-  {
-    students {
-      firstName
-      lastName
-      age
+  const fetchData = async () => {
+    setLoading(true)
+    const response = await fetch(
+      "https://api-ap-south-1.hygraph.com/v2/clbnux5qh1k8r01t8b9jnhrco/master",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          query: `
+          query Students {
+            students {
+              firstName
+              lastName
+              age
+            }
+          }
+      `,
+        }),
+      }
+    )
+
+    console.log(response)
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!!!")
+    } else {
+      return response.json()
     }
   }
-`
 
-const QUERY_CREATE = apollo`
-  mutation createUser(
-    $firstName: String!
-    $secondName: String!
-    $age: String!
-  ) {
-    createUser(firstName: $firstName, secondName: $secondName, age: $age) {
-      firstName
-      lastName
-      age
-    }
-  }
-`
+  useEffect(() => {
+    fetchData()
+      .then((data) => setStudents(data.data.students))
+      .catch((err) => console.log("Error --> ", err))
+      .finally(() => setLoading(false))
+  }, [])
 
-export default function Home({ students }) {
-  console.log(students)
+  // useEffect(() => {
+  //   fetch(
+  //     "https://api-ap-south-1.hygraph.com/v2/clbnux5qh1k8r01t8b9jnhrco/master",
+  //     {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         query: `
+  //           query Students {
+  //             students {
+  //               firstName
+  //               lastName
+  //               age
+  //             }
+  //           }
+  //       `,
+  //       }),
+  //     }
+  //   )
+  //     .then((res) => {
+  //       console.log(res)
+  //       return res.json()
+  //     })
+  //     .then((data) => setStudents(data.data.students))
+  // }, [])
+
+  // useEffect(() => {
+  //   fetch(
+  //     "https://api-ap-south-1.hygraph.com/v2/clbnux5qh1k8r01t8b9jnhrco/master",
+  //     {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         query: `
+  //         mutation MyMutation {
+  //           createStudent(data: {firstName: "Test name 255", lastName: "Last name 2", age: "29"}) {
+  //           }
+  //         }
+  //       `,
+  //       }),
+  //     }
+  //   )
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err))
+  // }, [])
 
   return (
     <div className="container">
@@ -86,19 +141,25 @@ export default function Home({ students }) {
                   <TableCell sx={{ fontWeight: 700 }}>Age</TableCell>
                 </TableRow>
               </TableHead>
-              <TableBody>
-                {students.map((item, index) => (
-                  <TableRow
-                    key={index}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell>{index + 1}</TableCell>
-                    <TableCell>{item.firstName}</TableCell>
-                    <TableCell>{item.lastName}</TableCell>
-                    <TableCell>{item.age}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
+              {loading ? (
+                <div className="loading">Loading...</div>
+              ) : (
+                <TableBody>
+                  {students?.map((item, index) => (
+                    <TableRow
+                      key={index}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 },
+                      }}
+                    >
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{item.firstName}</TableCell>
+                      <TableCell>{item.lastName}</TableCell>
+                      <TableCell>{item.age}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              )}
             </Table>
           </TableContainer>
         </div>
@@ -107,12 +168,12 @@ export default function Home({ students }) {
   )
 }
 
-export async function getStaticProps() {
-  const { students } = await graphcms.request(QUERY)
+// export async function getStaticProps() {
+//   const { students } = await graphcms.request(QUERY)
 
-  return {
-    props: {
-      students,
-    },
-  }
-}
+//   return {
+//     props: {
+//       students,
+//     },
+//   }
+// }
